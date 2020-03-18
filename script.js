cb4dc712f90d8999579832a7452c9f71e2490d19
Majd Sufian <majdnewsufian@gmail.com>
2020-03-18 13:24:54 +0100

    convert the code to ES6

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
-/**
+  /**
  * DONE: Update the text in the "Formatted Text" section as a user types in the textarea
  * DONE: Add a .bold, .italic classes to "Formatted Text" when the appropriate button is clicked
  * document: Add an .underline class to "Formatted Text" when Underline button is clicked
  * DONE: Toggle the align style for "Formatted Text" when the appropriate button is clicked
  */
 
-function updateText(){
-  var textInput = document.getElementById("text-input").value
+
+updateText = () => {
+  let textInput = document.getElementById("text-input").value
   document.getElementById("text-output").innerHTML = textInput;
 }
 
 
-function makeBold(elem){
-  //CODE GOES HERE
+makeBold = (elem) => {
   elem.classList.toggle('active-d')
   document.getElementById('text-output').classList.toggle('font-weight-bold')
 }
 
 
-function makeItalic(elem){
+makeItalic = (elem) => {
   elem.classList.toggle('active-d')
   document.getElementById('text-output').classList.toggle('font-italic')
 }
 
 
-function makeUnderline(elem){
+makeUnderline = (elem) => {
   elem.classList.toggle('active-d')
   let addClass = document.getElementById('text-output')
   if (addClass.classList.contains('underline')){
@@ -35,41 +35,23 @@ function makeUnderline(elem){
 }
 
 
-function alignText(elem, alignType){
+alignText = (elem, alignType) => {
   document.getElementById('text-output').style.textAlign = alignType
   // adding a new variable for buttons
   // adding a for loop in order to emplempt the logic
   let buttonsList = document.getElementsByClassName('align')
-  for(let i = 0; i < buttonsList.length; i++){
-    buttonsList[i].classList.remove('active-d')
+  for(let button of buttonsList){
+    button.classList.remove('active-d')
   }
   elem.classList.add('active-d')
 }
 
 
-function fontText(elem, textTransform){
+fontText = (elem, textTransform) => {
   document.getElementById('text-output').style.textTransform = textTransform
   let fontText =  document.getElementsByClassName('font')
-  for(let i = 0; i < fontText.length; i++){
-    fontText[i].classList.remove('active-d')
+  for(let font of fontText){
+    font.classList.remove('active-d')
   }
   elem.classList.add('active-d')
 }
-
-
-
-// function makeLowercase(elem){
-//   elem.classList.toggle('active-d')
-//   document.getElementById('text-output').classList.toggle('text-lowercase')
-// }
-
-// function makeUppercase(elem){
-//   elem.classList.toggle('active-d')
-//   document.getElementById('text-output').classList.toggle('text-uppercase')
-// }
-
-// function makeCapitalize(elem){
-//   elem.classList.toggle('active-d')
-//   document.getElementById('text-output').classList.toggle('text-capitalize')
-// }
-
